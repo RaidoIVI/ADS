96c2591eb8474bb47180082b1ded9bc46f352d59
Ivan <kiba82@gmail.com>
2022-06-14 00:20:49 +0300

    tmp

--- Launcher/Launcher.cs
+++ Launcher/Launcher.cs
@@ -4,25 +4,25 @@ namespace Launcher
 {
     public class HW
     {
-        private static List<IHomeWork> homeWorks = new();
+        private static List<IHomeWork?> homeWorks = new();
 
         public static String[] GetList()
         {
-            List<Type> HomeWorkList = new();
-            HomeWorkList.AddRange((Assembly.GetCallingAssembly().GetTypes().Where(t => t.GetInterfaces()
+            List<Type> homeWorkList = new();
+            homeWorkList.AddRange((Assembly.GetCallingAssembly().GetTypes().Where(t => t.GetInterfaces()
             .Where(i => i == typeof(IHomeWork)).ToList().Count > 0)));
             List<string> tmp = new();
-            for (int i = 0; i < HomeWorkList.Count; i++)
+            for (int i = 0; i < homeWorkList.Count; i++)
             {
-                homeWorks.Add((IHomeWork)Activator.CreateInstance(HomeWorkList[i]));
-                tmp.Add($" {homeWorks[i].Lession} {homeWorks[i].Name} {homeWorks[i].Description}");
+                homeWorks.Add((IHomeWork)Activator.CreateInstance(homeWorkList[i])!);
+                tmp.Add($" {homeWorks[i]?.Lession} {homeWorks[i]?.Name} {homeWorks[i]?.Description}");
             }
             return tmp.ToArray();
         }
 
         public static void Run(int value)
         {
-            homeWorks[value].Run();
+            homeWorks[value]?.Run();
         }
     }
 }
